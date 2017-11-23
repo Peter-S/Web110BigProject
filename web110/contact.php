0b42e3019044b28189d41b4d8c19a19b44b72ead
Peter-S <lmnopbrunos11@gmail.com>
2017-11-22 21:01:31 -0800

    Fixes
    Added some content but mostly fixed html errors btw i lost a page oops

--- web110/contact.php
+++ web110/contact.php
@@ -14,7 +14,6 @@
         <header>
         <h1>Pete Soukup's Web Portal</h1>
         <nav class="topnav" id="myTopnav">
-            <ul>
                <a href="index.html">Welcome</a>
                <a href="aia.html">Audience, Issues and Approach</a>
                 <a href="big/index.html">Big</a>
@@ -22,17 +21,16 @@
                 <a href="fp/index.html">Final Project</a>
                 <a href="contact.php">Contact Peter</a>
                 <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-            </ul>
         </nav>
-        <header/>
+            </header>
             <h2>Contact Peter Soukup</h2>
             <?php include "includes/simple.php";?>
-            <p class="clear-recaptcha"></p>   
+            <p class="clear-recaptcha"></p>
             
     <footer>
         <small>&copy; 2017, Pete Soukup, All Rights Reserved, <a href="http://validator.w3.org/check?uri=referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">Valid CSS</a></small>  
     </footer>
-  
+        </div>
     <script>
         function myFunction() {
             var x = document.getElementById("myTopnav");
@@ -43,5 +41,8 @@
             }
         }
       </script>
+    </body>
+</html>
+
             
             
