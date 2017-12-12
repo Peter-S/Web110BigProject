ad0889e9db65a6034073b4375f1d0deabc183a5d
Peter-S <lmnopbrunos11@gmail.com>
2017-12-12 13:11:06 -0800

    last commit

---
+++ web110/fp/contact.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang=en> 
+  <head>
+    <title>CaesArts</title>
+    <meta name="description" content="SCC WEB110 Student Portal for Peter Soukup" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <meta name="robots" content="noindex,nofollow" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="css/caesarts.css" /> 
+      <link rel="stylesheet" href="css/lightcaseatr.css" />
+      <link rel="stylesheet" href="css/contact-form.css" />
+      <link href="https://fonts.googleapis.com/css?family=Shrikhand" rel="stylesheet">
+  </head>
+    <header>
+        
+    <h4 style="text-align: center;"><a href="index.html"><b>CaesArts</b></a></h4>
+        
+    
+    <nav class="topnav" id="myTopnav">
+      <a href="index.html">Welcome</a>
+      <a href="art1.html">&emsp;&emsp;Art 1&emsp;&emsp;</a>
+      <a href="art2.html">&emsp;&emsp;Art 2&emsp;&emsp;</a>
+      <a href="art3.html">&emsp;&emsp;Art 3&emsp;&emsp;</a>
+      <a href="art4.html">&emsp;&emsp;Art 4&emsp;&emsp;</a>
+      <a href="art5.html">&emsp;&emsp;Art 5&emsp;&emsp;</a>
+      <a href="flash.html">&emsp;&emsp;Flash&emsp;&emsp;</a>
+      <a href="contact.php">Contact Baby Beef</a>
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+    </nav>
+    </header>
+            <h2>CaesArts Directly</h2>
+            <?php include "includes/multiple.php";?>
+            <p class="clear-recaptcha"></p>
+            
+    <footer>
+        <small>&copy; 2017, Pete Soukup, All Rights Reserved, <a href="http://validator.w3.org/check?uri=referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">Valid CSS</a></small>  
+    </footer>
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+      </script>
+</html>
+
+            
+            
--- web110/fp/includes/multiple.php
+++ web110/fp/includes/multiple.php
@@ -18,9 +18,9 @@
  */
 
 #EDIT THE FOLLOWING:
-$toAddress = "yourname@example.com";  //place your/your client's email address here
-$toName = "CLIENT NAME HERE"; //place your client's name here
-$website = "CLIENT WEBSITE NAME HERE";  //place NAME of your client's website here
+$toAddress = "lmnopbrunos11@gmail.com";  //place your/your client's email address here
+$toName = "CaesArts"; //place your client's name here
+$website = "http://www.CaesArts.com";  //place NAME of your client's website here
 #--------------END CONFIG AREA ------------------------#
 $sendEmail = TRUE; //if true, will send an email, otherwise just show user data.
 $dateFeedback = true; //if true will show date/time with reCAPTCHA error - style a div with class of dateFeedback
